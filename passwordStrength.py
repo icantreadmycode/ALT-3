0805b4c4cd35fb411b084c2fd9f9dddb12721321
icantreadmycode <joshuabrennan2007@gmail.com>
2023-09-28 18:17:00 +0100

    todo: impliment some functions

--- passwordStrength.py
+++ passwordStrength.py
@@ -1,9 +1,13 @@
-# Special Characters
-# !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+# Accepted characters (ASCII)
+# 0123456789
+# abcdefghijklmnopqrstuvwxyz
+# ABCDEFGHIJKLMNOPQRSTUVWXYZ
+# !"#$%&\'()*+,-./:;<=>?@[]^_`{|}~
 
 from dataclasses import dataclass
+import os
 
-running = True
+specialChars = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 @dataclass
 class passwordRequirements:
@@ -19,7 +23,19 @@ def handleNotInt(x):
     except Exception as err:
         print(f"Bad input: input only numbers.")
 
+def calculateStrength():
+    # TODO
+    # num of potential chars ^ num of min chars
+    # adjust for required capitals (at least 1)
+    # adjust for numbers required (at least 1)
+    return 1 # this will be the number of possible password that fit the requirements (minimum)
+
+def calculateTime():
+    # calculate rough time to guess the password based on output of {calculateStrength}
+    return 1
+
 if __name__ == "__main__":  
+    running = True
     # take in password requirements
     while running:
         passwordRec = passwordRequirements(
@@ -28,12 +44,15 @@ if __name__ == "__main__":
             True if input("Require capital letters? (Y/n) ") != "n" else False,
             handleNotInt(input("Input minimum password length: "))
         )
+        print("")
 
-        print(passwordRec)
+        calculateStrength()
+        calculateTime()
 
-        if input("Exit (Y/n)") == "n":
-            None
+        if input("Exit (Y/n) ") == "n":
+            os.system('cls')
         else:
+            os.system('cls')
             running = False
 
 
