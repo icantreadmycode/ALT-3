1eec4d783f3a305ba6cc530873ab1dcf2531dd0d
icantreadmycode <joshuabrennan2007@gmail.com>
2023-09-29 12:19:59 +0100

    progress

--- passwordStrength.py
+++ passwordStrength.py
@@ -3,11 +3,16 @@
 # abcdefghijklmnopqrstuvwxyz
 # ABCDEFGHIJKLMNOPQRSTUVWXYZ
 # !"#$%&\'()*+,-./:;<=>?@[]^_`{|}~
+# abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~
 
 from dataclasses import dataclass
 import os
+import math
 
 specialChars = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+allAlpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+capitalAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+nums = "0123456789"
 
 @dataclass
 class passwordRequirements:
@@ -23,16 +28,63 @@ def handleNotInt(x):
     except Exception as err:
         print(f"Bad input: input only numbers.")
 
-def calculateStrength():
+def product(inputlist: list):
+    result = 1
+    for i in range(len(inputlist)):
+        result *= inputlist[i]
+    return result
+
+
+def calculateStrength(passRec: passwordRequirements):
     # TODO
     # num of potential chars ^ num of min chars
     # adjust for required capitals (at least 1)
     # adjust for numbers required (at least 1)
-    return 1 # this will be the number of possible password that fit the requirements (minimum)
+    possiblities = [0]*passRec.minPasswordLength
+    for i in range(passRec.minPasswordLength):
+        if passRec.requireCapitals and i > 1:
+            possiblities[i] = len(capitalAlpha)
+        if passRec.requireNumbers and i > 2:
+            possiblities[i] = len(nums)
+        if passRec.requireSpecialCharacters and i > 3:
+            possiblities[i] = len(specialChars)
+        else:
+            # print(f"""
+            #     all: {allAlpha} {len(allAlpha)}
+            #     cap: {capitalAlpha} {len(capitalAlpha)}
+            #     nums: {nums} {len(nums)}
+            #     spec: {specialChars} {len(specialChars)}
+            # """)
+            possiblities[i] = len(allAlpha) + len(nums) + len(specialChars)
+    
+    return product(possiblities)
 
-def calculateTime():
-    # calculate rough time to guess the password based on output of {calculateStrength}
-    return 1
+def calculateTime(permutations):
+    # calculate rough time to guess the password based on output of {calculateStrength} 10 per second
+    seconds = permutations
+    minutes = 0
+    hours = 0
+    days = 0
+    if seconds < (60**2)*24:
+        if seconds < 60**2:
+            if seconds < 60:
+                pass
+            else:
+                minutes = math.floor(seconds / 60)
+                seconds = seconds - (minutes * 60)
+        else:
+            minutes = math.floor(seconds / 60)
+            hours = math.floor(minutes / 60)
+            seconds = seconds - (minutes * 60)
+            minutes = minutes - (hours * 60)
+    else:
+        minutes = math.floor(seconds / 60)
+        hours = math.floor(minutes / 60)
+        days = math.floor(hours / 24)
+        
+
+    return f"{days}d: {hours}h: {minutes}m: {seconds}s"        
+
 
 if __name__ == "__main__":  
     running = True
@@ -46,8 +98,9 @@ if __name__ == "__main__":
         )
         print("")
 
-        calculateStrength()
-        calculateTime()
+        perm = calculateStrength(passwordRec)
+        print("number of permuatitions: ", perm)
+        print(calculateTime(perm))
 
         if input("Exit (Y/n) ") == "n":
             os.system('cls')
