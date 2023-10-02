82de25c7eb50fc2f8abea6dcfb883f204a2bc60d
icantreadmycode <joshuabrennan2007@gmail.com>
2023-10-02 14:28:13 +0100

    added comment descriptions

--- passwordStrength.py
+++ passwordStrength.py
@@ -26,7 +26,7 @@ def handleNotInt(x, default: int):
     # handle int to str conversion
     try:
         return int(x)
-    except ValueError as err:
+    except ValueError: # catch error (Value error specifically)
         print(f"Bad input: {x} is not valid. input whole numbers only. defaulting to {default}")
         return default
 
@@ -41,15 +41,15 @@ def product(inputlist: list):
 def calculateStrength(passRec: passwordRequirements):
     # calculate the number of possible permutations the given requirements can be arranged
     possiblities = [0]*passRec.minPasswordLength
-    for i in range(passRec.minPasswordLength):
+    for i in range(passRec.minPasswordLength): # loop over the length of the minPasswordLength
         if passRec.requireCapitals and i == 1:
-            possiblities[i] = len(capitalAlpha)
+            possiblities[i] = len(capitalAlpha) 
         elif passRec.requireNumbers and i == 2:
             possiblities[i] = len(nums)
         elif passRec.requireSpecialCharacters and i == 3:
             possiblities[i] = len(specialChars)
         else:
-            possiblities[i] = len(allAlpha) + len(nums) + len(specialChars)
+            possiblities[i] = len(allAlpha) + len(nums) + len(specialChars) # calculate the total amount of characters when all can be used
     
     return product(possiblities)
 
@@ -59,20 +59,20 @@ def calculateTime(permutations, persec):
     minutes = 0
     hours = 0
     days = 0
-    if seconds < (60**2)*24:
-        if seconds < 60**2:
-            if seconds < 60:
+    if seconds < (60**2)*24: # if there is less than a days worth of seconds
+        if seconds < 60**2: # if there is less than an hours worth of seconds
+            if seconds < 60: # if there is less than a minutes worth of seconds
                 pass
             else:
-                minutes = math.floor(seconds / 60)
+                minutes = math.floor(seconds / 60) # calculate minutes and seconds
                 seconds = seconds - (minutes * 60)
         else:
-            minutes = math.floor(seconds / 60)
+            minutes = math.floor(seconds / 60) # calculate hours, minutes and seconds
             hours = math.floor(minutes / 60)
             seconds = seconds - (minutes * 60)
             minutes = minutes - (hours * 60)
     else:
-        minutes = math.floor(seconds / 60)
+        minutes = math.floor(seconds / 60) # calculate days, hours, minutes and seconds
         hours = math.floor(minutes / 60)
         days = math.floor(hours / 24)
         hours = hours - days*24
